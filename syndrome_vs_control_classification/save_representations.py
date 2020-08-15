0d13ae59c48e0306f47ea76b1eb2d3b289022ede
Ockers, F. (Fien) <f.ockers@student.ru.nl>
2020-08-15 12:14:06 +0200

    reformating code and including documentation

---
+++ syndrome_vs_control_classification/save_representations.py
@@ -0,0 +1,23 @@
+import openface_cfps
+import deepface
+import facereader
+
+def main(GENERAL_DIR, syn_list):
+
+    for syn in syn_list:
+        print(syn, "\n")
+        
+        print ("openface and cfps")
+        openface_cfps.openface_cfps_reps(GENERAL_DIR, syn)
+
+        print ("deepface")
+        deepface.deepface_reps(GENERAL_DIR, syn)
+    
+        print ("facereader-landmarks")
+        facereader.facereader_landmarks_reps(GENERAL_DIR, syn) 
+
+        print ("facereader-landmarks-distances")
+        facereader.facereader_landmarks_dis_reps(GENERAL_DIR, syn) 
+        
+    print("Done running save_representations.py")
+
